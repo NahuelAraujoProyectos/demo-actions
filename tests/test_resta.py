ef786b39310d71860fc671e867e5ad8a551fc092
Nahuel Araujo <nahuelaraujo1991@gmail.com>
2024-07-24 19:37:23 +0200

    corrección

--- tests/test_resta.py
+++ tests/test_resta.py
@@ -5,7 +5,7 @@ from proyecto.resta import resta
 class TestResta(unittest.TestCase):
     def test_resta(self):
         self.assertEqual(resta(5,4),1)
-        self.assertEqual(resta(2,3),1)
+        self.assertEqual(resta(2,3),-1)
         self.assertEqual(resta(6,6),0)
 
 if __name__ == '__main__':
